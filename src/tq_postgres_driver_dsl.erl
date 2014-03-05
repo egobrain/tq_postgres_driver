c3a57fe149aa7f4f1247ded56fea1d2b6e89139e
Egobrain <xazar.studio@gmail.com>
2014-03-05 16:36:16 +0400

    New dsl features. Moved to tq_db 0.5.0

--- src/tq_postgres_driver_dsl.erl
+++ src/tq_postgres_driver_dsl.erl
@@ -17,7 +17,19 @@
 %% =============================================================================
 
 parse(Model, Bin) ->
-    tq_dsl_parser:parse(Bin, fun sql_joiner/3, #state{model=Model}).
+    Res = tq_dsl_parser:parse(Bin, fun sql_joiner/3, #state{model=Model}),
+    case Res of
+        {error, {wrong_format, {Pos, _Reasin}}} ->
+            case Bin of
+                <<From:Pos/binary, To/binary>> ->
+                    Err = <<From/binary, "\e[31m", To/binary, "\e[0m">>,
+                    io:format("~s~n", [Err]);
+                _ -> ok
+            end;
+        _ ->
+            ok
+    end,
+    Res.
 
 %% =============================================================================
 %%% Internal functions
@@ -27,11 +39,11 @@ sql_joiner({string, _Pos, Str}, #state{acc=Acc}=State, Next) ->
     Acc2 = <<Acc/binary, Str/binary>>,
     Next(State#state{acc=Acc2});
 sql_joiner({field_query, Pos, FieldQuery}, State, Next) ->
-    field_query(<<>>, FieldQuery, false, Pos, State, Next);
-sql_joiner({field_query_alias, Pos, FieldQuery}, State, Next) ->
-    field_query(<<>>, FieldQuery, true, Pos, State, Next);
-sql_joiner({field_alias, _Pos, MF}, State, Next) ->
-    field_alias(<<>>, MF, _Pos, State, Next);
+    field_query(none, FieldQuery, false, Pos, State, Next);
+sql_joiner({field_query_alias, Pos, Link, FieldQuery}, State, Next) ->
+    field_query(Link, FieldQuery, true, Pos, State, Next);
+sql_joiner({field_alias, _Pos, Link, MF}, State, Next) ->
+    field_alias(Link, MF, _Pos, State, Next);
 sql_joiner({field_type, _Pos, MF}, #state{model=DefaultModel}=State, Next) ->
     {Model, Field} = get_model_and_field(MF, DefaultModel),
     Type = Model:'$meta'({db_type, Field}),
@@ -42,12 +54,6 @@ sql_joiner({table, _Pos, BinModel}, #state{acc=Acc}=State, Next) ->
     Table = Model:'$meta'(table),
     Acc2 = <<Acc/binary, Table/binary>>,
     Next(State#state{acc=Acc2});
-sql_joiner({table_link, _Pos, TableLink, {field_query, Pos, FieldQuery}}, State, Next) ->
-    field_query(<<$", TableLink/binary, $", $.>>, FieldQuery, false, Pos, State, Next);
-sql_joiner({table_link, _Pos, TableLink, {field_query_alias, Pos, FieldQuery}}, State, Next) ->
-    field_query(<<$", TableLink/binary, $", $.>>, FieldQuery, true, Pos, State, Next);
-sql_joiner({table_link, _Pos, TableLink, {field_alias, _Pos2, MF}}, State, Next) ->
-    field_alias(<<$", TableLink/binary, $", $.>>, MF, _Pos, State, Next);
 sql_joiner({type, _Pos, BinType}, State, Next) ->
     Type = binary_to_atom(BinType),
     TypeWrapper = fun(Val) -> {Type, Val} end,
@@ -64,8 +70,9 @@ get_model_and_field(BinField, DefaultModel) when is_binary(BinField) ->
 get_model_and_field(AtomField, DefaultModel) when is_atom(AtomField) ->
     {DefaultModel, AtomField}.
 
-field_alias(TableLink, MF, Pos, #state{acc=Acc, model=DefaultModel} = State, Next) ->
+field_alias(Link, MF, Pos, #state{acc=Acc, model=DefaultModel} = State, Next) ->
     {Model, Field} = get_model_and_field(MF, DefaultModel),
+    TableLink = link_to_sql(Link, Model),
     case Model of
         undefined ->
             {error, {wrong_format, {Pos, "Unknown model"}}};
@@ -90,41 +97,45 @@ field_alias(TableLink, MF, Pos, #state{acc=Acc, model=DefaultModel} = State, Nex
             Next(State2)
     end.
 
-field_query(TableLink, '...', _Expr, _Pos,
+field_query(Link, '...', _Expr, _Pos,
             #state{acc=Acc, fields=Fields, model=Model}=State, Next) ->
     DbFields = Model:'$meta'({db_fields, r}),
     ResultFields = DbFields -- Fields,
+    TableLink = link_to_sql(Link, Model),
     FieldsSql = join_fields(TableLink, Model, ResultFields),
     State2 = State#state{
                acc = <<Acc/binary, FieldsSql/binary>>,
                fields = Fields ++ ResultFields
               },
     Next(State2);
-field_query(TableLink, '*', _IsAlias, _Pos,
+field_query(Link, '*', _IsAlias, _Pos,
             #state{acc=Acc, fields=Fields, model=Model}=State, Next) ->
     DbFields = Model:'$meta'({db_fields, r}),
+    TableLink = link_to_sql(Link, Model),
     FieldsSql = join_fields(TableLink, Model, DbFields),
     State2 = State#state{
                acc = <<Acc/binary, FieldsSql/binary>>,
                fields = Fields ++ DbFields
               },
     Next(State2);
-field_query(TableLink, {BinModel, BinField}, IsAlias, Pos,
+field_query(Link, {BinModel, BinField}, IsAlias, Pos,
             #state{model=Model}=State, Next) ->
     M = binary_to_atom(BinModel),
     case M of
         Model ->
+            TableLink = link_to_sql(Link, M),
             field_query(TableLink, BinField, IsAlias, Pos, State, Next);
         _ ->
             {error, {wrong_model, {Pos, "Querying data from another model not supported"}}}
     end;
-field_query(TableLink, BinField, IsAlias, _Pos,
+field_query(Link, BinField, IsAlias, _Pos,
             #state{acc=Acc, fields=Fields, model=Model}=State, Next)
   when is_binary(BinField) ->
     Field = binary_to_atom(BinField),
     State2 =
         case IsAlias of
             true ->
+                TableLink = link_to_sql(Link, Model),
                 FieldSql = field_to_sql(TableLink, Model, Field),
                 State#state{
                   acc = <<Acc/binary, FieldSql/binary>>
@@ -153,6 +164,10 @@ join_fields(_TableLink, _Model, []) ->
 join_fields(TableLink, Model, Fields) ->
     <<$,, Str/binary>> = << <<$,, (field_to_sql(TableLink, Model, F))/binary>> || F <- Fields >>,
     Str.
+
+link_to_sql(none, _Model) -> <<>>;
+link_to_sql(table, Model) -> <<(Model:'$meta'(table))/binary, $.>>;
+link_to_sql({_Pos, Link}, _Model) -> <<$", Link/binary, $", $.>>.
 
 field_to_sql(TableLink, Model, Field) ->
     <<TableLink/binary, (Model:'$meta'({db_alias, Field}))/binary>>.
